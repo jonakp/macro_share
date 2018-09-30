0ec07a366724fe9c16bc8dfdec2513415ee8b21d
kpb <email@example.com>
2018-09-30 10:54:40 +0900

    #7 finish modify title output

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def title(page_title)
+    base_title = 'Macro Share'
+    if page_title.empty?
+      base_title
+    else
+      page_title + '| Macro Share'
+    end
+  end
 end
