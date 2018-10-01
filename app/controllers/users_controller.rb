ab16488fe1a8b006c0fb664057859fc7574dd3bf
kpb <email@example.com>
2018-10-02 06:25:24 +0900

    finish add edit user except for image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,21 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params) 
+      flash[:success] = 'ユーザ更新が完了しました'
+      log_out
+      redirect_to login_path 
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def user_params
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_25_140408) do
+ActiveRecord::Schema.define(version: 2018_10_01_210527) do
 
   create_table "userfeatures", force: :cascade do |t|
     t.integer "user_id"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2018_09_25_140408) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_name"
   end
 
 end
